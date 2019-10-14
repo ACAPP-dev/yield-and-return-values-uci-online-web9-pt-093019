5442f799b1a241a342f95973d697339d3541b1e0
Andrew Capp <acapp909@gmail.com>
2019-10-14 02:16:56 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,10 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
-    new_array = yield(array[i])
+    new_array << yield(array[i])
     i += 1
   end
-  new_array
+  p new_array
 end
 
 binding.pry
