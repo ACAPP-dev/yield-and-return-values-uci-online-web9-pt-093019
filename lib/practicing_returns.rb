e2e6dd6f949bf261b825e392db211b8a97e1d3e6
Andrew Capp <acapp909@gmail.com>
2019-10-14 02:19:42 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,11 @@ def hello(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << yield(array[i])
+    yield(array[i])
     i += 1
   end
   p new_array
 end
 
 #binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
